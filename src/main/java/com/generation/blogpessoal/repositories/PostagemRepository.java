1ed8e0463dca024fe8b53065679d37d3c0c2a39b
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-01-30 13:21:17 -0300

    Anotacao adicionada na PostagemRepository

--- src/main/java/com/generation/blogpessoal/repositories/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repositories/PostagemRepository.java
@@ -2,7 +2,9 @@ package com.generation.blogpessoal.repositories;
 
 import com.generation.blogpessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
 }
