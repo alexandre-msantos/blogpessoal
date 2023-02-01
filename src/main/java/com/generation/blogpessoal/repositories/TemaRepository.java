df8cc6c3503174c578a9c6b9b0413d5ae0c882e3
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-02-01 09:47:46 -0300

    TemaRepository implementado

---
+++ src/main/java/com/generation/blogpessoal/repositories/TemaRepository.java
@@ -0,0 +1,9 @@
+package com.generation.blogpessoal.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TemaRepository extends JpaRepository {
+
+}
